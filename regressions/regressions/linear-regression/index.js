b03891cda8645464fa5d9d4f750782264cb11884
Christopher Jolly <chris.jolly@bachcare.co.nz>
2021-11-30 17:28:19 +1300

    Refactor to reuse regression.

--- regressions/regressions/linear-regression/index.js
+++ regressions/regressions/linear-regression/index.js
@@ -4,9 +4,9 @@ const plot = require('node-remote-plot')
 
 const tf = require('@tensorflow/tfjs')
 
-const loadCSV = require('./load-csv')
+const loadCSV = require('../load-csv')
 
-let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
+let { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
   shuffle: true,
   splitTest: 50,
   dataColumns: ['horsepower', 'weight', 'displacement'],
